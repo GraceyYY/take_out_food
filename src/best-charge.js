fae76279de79f2b90766b0ff5319968bf54c5c59
Grace <lunay0325@gmail.com>
2018-12-17 14:41:36 +0800

    Complete the function that can get item infomation by id

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,13 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  return;
+}
+
+function getItemInfo(itemId) {
+    let items = loadAllItems();
+
+    for(let item of items) {
+        if(item.id === itemId) {
+            return item;
+        }
+    }
 }
