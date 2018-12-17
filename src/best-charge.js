1e1d428c8e116841a36c209c4d51c2a37caa62fa
Grace <lunay0325@gmail.com>
2018-12-17 14:54:53 +0800

    Complete the function that calculates the full price of the order

--- src/best-charge.js
+++ src/best-charge.js
@@ -3,11 +3,19 @@ function bestCharge(selectedItems) {
 }
 
 function getItemInfo(itemId) {
-    let items = loadAllItems();
-
-    for(let item of items) {
-        if(item.id === itemId) {
-            return item;
-        }
+  let items = loadAllItems();
+  for (let item of items) {
+    if (item.id === itemId) {
+      return item;
     }
+  }
 }
+
+function calculateFullPrice(selectedItems) {
+  return selectedItems.reduce((fullprice, item) => {
+    let id = item.split(' ')[0];
+    let num = parseInt(item.split(' ')[2]);
+    return fullprice + getItemInfo(id).price * num;
+  }, 0);
+}
+
